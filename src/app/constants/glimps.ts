b0c5a07eeeeeb10ad944c6c2a373a5464a051b10
Shreyapatil2142 <shreyapatil2142@gmail.com>
2026-03-13 17:21:30 +0530

    Gallary winners added

--- src/app/constants/glimps.ts
+++ src/app/constants/glimps.ts
@@ -9,27 +9,12 @@ export interface Glimpse {
 }
 
 export const glimpses: Glimpse[] = [
+  
   {
     id: 1,
-    title: "RoboRashtra 2k25",
-    date: "Feb 2025",
-    image: "/glimps/robo1.JPG",
-    description: "Epic battles between autonomous fighting robots",
-    stats: "32+ Teams",
-    images: [
-      "/glimps/img.jpg",
-      "/glimps/robo2.JPG",
-      "/glimps/robo3.JPG",
-      "/glimps/robo4.JPG",
-      "/glimps/robo5.JPG",
-      "/glimps/robo6.JPG",
-    ],
-  },
-  {
-    id: 2,
     title: "YantraUtsav",
     date: "Feb 2025",
-    image: "/glimps/1.jpg",
+    image: "/glimps/robo3.jpg",
     description: "Cutting-edge AI demonstrations and breakthroughs",
     stats: "150+ Participants",
     images: [
@@ -38,24 +23,40 @@ export const glimpses: Glimpse[] = [
       "/glimps/yan3.JPG",
       "/glimps/yan4.JPG",
       "/glimps/yan5.JPG",
-      "/glimps/yan6.JPG",
+      "/glimps/yan6.jpg",
      
     ],
   },
   {
-    id: 3,
+    id: 2,
     title: "ResQlypmic",
     date: "Feb 2025",
-    image: "/glimps/4.jpg",
+    image: "/glimps/res5.jpg",
     description: "High-speed autonomous drone racing through obstacle courses",
     stats: "64+ Teams",
     images: [
-      "/glimps/res1.JPG",
-      "/glimps/res2.JPG",
-      "/glimps/res3.JPG",
-      "/glimps/res4.JPG",
-      "/glimps/res5.JPG",
-      "/glimps/res6.JPG",
+      "/glimps/res1.jpg",
+      "/glimps/res2.jpg",
+      "/glimps/res3.jpg",
+      "/glimps/res4.jpg",
+      "/glimps/res5.jpg",
+      "/glimps/res6.jpg",
+    ],
+  },
+  {
+    id:3,
+    title: "Chakravyuh",
+    date: "Feb 2025",
+    image: "/glimps/chak4.jpg",
+    description: "High-speed autonomous drone racing through obstacle courses",
+    stats: "64+ Teams",
+    images: [
+      "/glimps/chak1.jpg",
+      "/glimps/chak2.jpg",
+      "/glimps/chak3.jpg",
+      "/glimps/chak4.jpg",
+      "/glimps/chak5.jpg",
+      "/glimps/chak1.jpg",
     ],
   },
 ];
